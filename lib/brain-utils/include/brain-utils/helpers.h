dc9a6c16ee9c2cc218f485b0e69f1f70af7ae797
Peter Zimon <peter.zimon@gmail.com>
2025-12-29 18:32:45 +0100

    Added clamp to helpers

--- lib/brain-utils/include/brain-utils/helpers.h
+++ lib/brain-utils/include/brain-utils/helpers.h
@@ -1,3 +1,14 @@
+#include <stdint.h>
+
 static inline long map(long x, long in_min, long in_max, long out_min, long out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+static inline int clamp(int min, int max, int value) {
+	if (value < min) {
+		return min;
+	} else if (value > max) {
+		return max;
+	}
+	return value;
 }
